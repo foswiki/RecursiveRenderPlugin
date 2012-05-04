aac71489927fa8cdb9db0309d01ba207b6e6db1b
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/RecursiveRenderPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/RecursiveRenderPlugin.pm
+++ lib/TWiki/Plugins/RecursiveRenderPlugin.pm
@@ -14,7 +14,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -43,15 +43,14 @@
 #   getSessionValueHandler  ( $key )                                1.010  Use only in one Plugin
 #   setSessionValueHandler  ( $key, $value )                        1.010  Use only in one Plugin
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, all handlers except initPlugin are
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name. Remove disabled handlers you do not need.
 #
-# NOTE: To interact with TWiki use the official TWiki functions 
+# NOTE: To interact with TWiki use the official TWiki functions
 # in the TWiki::Func module. Do not reference any functions or
 # variables elsewhere in TWiki!!
-
 
 # =========================
 package TWiki::Plugins::RecursiveRenderPlugin;
@@ -61,10 +60,10 @@ use Data::Dumper;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug $TranslationToken $renderedText @renderStack
-        %macros $prefix $rprefix $rLevel
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+  $debug $TranslationToken $renderedText @renderStack
+  %macros $prefix $rprefix $rLevel
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -76,7 +75,7 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-$pluginName = 'RecursiveRenderPlugin';  # Name of this Plugin
+$pluginName = 'RecursiveRenderPlugin';    # Name of this Plugin
 
 # =========================
 sub writeDebug {
@@ -84,37 +83,39 @@ sub writeDebug {
 }
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        TWiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        TWiki::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
+    $debug = TWiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
 
     #initialize a few other things
-    @renderStack = ();
-    %macros = ();
+    @renderStack  = ();
+    %macros       = ();
     $renderedText = [];
-    $prefix = "_render_";
-    $rprefix = $prefix . "r_";
-    $rLevel = -1;
+    $prefix       = "_render_";
+    $rprefix      = $prefix . "r_";
+    $rLevel       = -1;
 
     # Plugin correctly initialized
-    writeDebug( "- TWiki::Plugins::RecursiveRenderPlugin::initPlugin( $web.$topic ) is OK" );
+    writeDebug(
+"- TWiki::Plugins::RecursiveRenderPlugin::initPlugin( $web.$topic ) is OK"
+    );
     return 1;
 }
 
 # =========================
-sub startRenderingHandler
-{
+sub startRenderingHandler {
+
     # my ( $text, $web, $meta ) = @_; do not uncomment, use $_[0]
-    writeDebug( "- ${pluginName}::startRenderingHandler( $_[1] )" );
+    writeDebug("- ${pluginName}::startRenderingHandler( $_[1] )");
 
     push @renderStack, $renderedText;
     $rLevel++;
@@ -123,84 +124,88 @@ sub startRenderingHandler
     # This handler is called by getRenderedVersion just before the line loop
 
     # read in ~macros
-    while ($_[0] =~ s/\n~([^\n]*)\n(.*?)\n~~(?=\n)//s) {
+    while ( $_[0] =~ s/\n~([^\n]*)\n(.*?)\n~~(?=\n)//s ) {
         my $macroName = $1;
-        my $macroText = TWiki::Func::renderText($2, $_[1], $_[2]);
-	$macroText =~ s/~(?=$macroName)/~<nop>/g; # no direct recursion
+        my $macroText = TWiki::Func::renderText( $2, $_[1], $_[2] );
+        $macroText =~ s/~(?=$macroName)/~<nop>/g;    # no direct recursion
         $macros{$macroName} = $macroText;
     }
+
     # check for recursive loops
     my @names = keys %macros;
     my @usageMatrix;
-    for (my $i = 0; $i < @names; $i++) {
-        my $macroText = $macros{$names[$i]};
-	for (my $j = 0; $j < @names; $j++) {
-	    if (index($macroText, "~$names[$j]") != -1) {
-		$usageMatrix[$i][$j] = 1;
-	    }
-	}
+    for ( my $i = 0 ; $i < @names ; $i++ ) {
+        my $macroText = $macros{ $names[$i] };
+        for ( my $j = 0 ; $j < @names ; $j++ ) {
+            if ( index( $macroText, "~$names[$j]" ) != -1 ) {
+                $usageMatrix[$i][$j] = 1;
+            }
+        }
     }
-    for (my $m = 0; $m < @names; $m++) {
-        for (my $s = 0; $s < @names; $s++) {
-	    for (my $e = 0; $e < @names; $e++) {
-	        if ($usageMatrix[$s][$m] && $usageMatrix[$m][$e]) {
-	            if ($s == $e) { #loop, break it
-		        writeDebug("- ${pluginName}::startRenderingHandler - breaking loop at $names[$s]");
-			$macros{$names[$s]} =~ s/~/~<nop>/g;
-			$usageMatrix[$s] = [];
-		    } else {
-			$usageMatrix[$s][$e] = 1;
-		    }
-		}
-	    }
-	}
+    for ( my $m = 0 ; $m < @names ; $m++ ) {
+        for ( my $s = 0 ; $s < @names ; $s++ ) {
+            for ( my $e = 0 ; $e < @names ; $e++ ) {
+                if ( $usageMatrix[$s][$m] && $usageMatrix[$m][$e] ) {
+                    if ( $s == $e ) {    #loop, break it
+                        writeDebug(
+"- ${pluginName}::startRenderingHandler - breaking loop at $names[$s]"
+                        );
+                        $macros{ $names[$s] } =~ s/~/~<nop>/g;
+                        $usageMatrix[$s] = [];
+                    }
+                    else {
+                        $usageMatrix[$s][$e] = 1;
+                    }
+                }
+            }
+        }
     }
 
     # read in <render> blocks
     my $renderCount = 0;
-    my $tag = "render";
-    my $newText = $_[0];
-    my $idx = rindex($newText, "<$tag>");
-    while ($idx != -1) {
-       my $endText = substr($newText, $idx);
-       $endText =~ s!<$tag>(.*?)</$tag>!$prefix$renderCount!is;
-       my $toRender = $1;
-       $toRender =~ s/$prefix/$rprefix/;
-       my $rText = TWiki::Func::renderText($toRender, $_[1], $_[2]);
-       $rText =~ s/$rprefix/$prefix/;
-       $$renderedText[$renderCount] = $rText;
-       $renderCount++;
-       $newText = substr($newText, 0, $idx) . $endText;
-       $idx = rindex($newText, "<$tag>");
+    my $tag         = "render";
+    my $newText     = $_[0];
+    my $idx         = rindex( $newText, "<$tag>" );
+    while ( $idx != -1 ) {
+        my $endText = substr( $newText, $idx );
+        $endText =~ s!<$tag>(.*?)</$tag>!$prefix$renderCount!is;
+        my $toRender = $1;
+        $toRender =~ s/$prefix/$rprefix/;
+        my $rText = TWiki::Func::renderText( $toRender, $_[1], $_[2] );
+        $rText =~ s/$rprefix/$prefix/;
+        $$renderedText[$renderCount] = $rText;
+        $renderCount++;
+        $newText = substr( $newText, 0, $idx ) . $endText;
+        $idx = rindex( $newText, "<$tag>" );
     }
     $_[0] = $newText;
-    
+
 }
 
 # =========================
-sub endRenderingHandler
-{
+sub endRenderingHandler {
 ### my ( $text ) = @_;   # do not uncomment, use $_[0] instead
 
-    writeDebug( "- ${pluginName}::endRenderingHandler( $web.$topic )" );
+    writeDebug("- ${pluginName}::endRenderingHandler( $web.$topic )");
 
-    if ($rLevel == 0) {
+    if ( $rLevel == 0 ) {
         my $doneWithMacros = 0;
-        MACROLOOP: until ($doneWithMacros) {
-	    $doneWithMacros = 1;
-	    foreach my $name (sort {length($b) <=> length($a)} keys %macros) {
-		if ($_[0] =~ s/~$name/$macros{$name}/ge) {
-		    $doneWithMacros = 0;
-		    next MACROLOOP;
-		}
-	    }
+      MACROLOOP: until ($doneWithMacros) {
+            $doneWithMacros = 1;
+            foreach my $name ( sort { length($b) <=> length($a) } keys %macros )
+            {
+                if ( $_[0] =~ s/~$name/$macros{$name}/ge ) {
+                    $doneWithMacros = 0;
+                    next MACROLOOP;
+                }
+            }
         }
     }
 
     if (@$renderedText) {
-        while ($_[0] =~ s/$prefix([0-9]+)/$$renderedText[$1]/e) {}
+        while ( $_[0] =~ s/$prefix([0-9]+)/$$renderedText[$1]/e ) { }
     }
-    
+
     $renderedText = pop @renderStack;
     $rLevel--;
 }
